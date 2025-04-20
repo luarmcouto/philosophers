3631b221a05ff533d1155236f3255e91463120a0
Lua Rodrigues Monteiro Couto <luarodri@c2r1s4.42porto.com>
2025-04-20 21:09:31 +0100

    chore: add TODO in main and update philo.h

--- main.c
+++ main.c
@@ -6,13 +6,22 @@
 /*   By: luarodri <luarodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 18:41:09 by luarodri          #+#    #+#             */
-/*   Updated: 2025/04/20 18:44:33 by luarodri         ###   ########.fr       */
+/*   Updated: 2025/04/20 20:54:14 by luarodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    
+	t_table		table;
+	t_philos	philosophers[MAX_PHILOS];
+
+	if (validate_arguments(ac, av))
+		return (1);
+	initialize_table();//TODO
+	initialize_all_philosophers();//TODO
+	start_dinner_simulation();//TODO
+	cleanup_simulation();//TODO
+	return (0);
 }
--- philo.h
+++ philo.h
@@ -6,28 +6,59 @@
 /*   By: luarodri <luarodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 18:37:56 by luarodri          #+#    #+#             */
-/*   Updated: 2025/04/20 18:44:31 by luarodri         ###   ########.fr       */
+/*   Updated: 2025/04/20 20:36:17 by luarodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifdef PHILO_H
+#ifndef PHILO_H
 # define PHILO_H
 
-#include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/time.h>
+// # include "colors.h"
+# include <pthread.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <stdlib.h>
+# include "terminal_palette.h"
 
-typedef struct  s_philo
+# define MAX_PHILOS 200
+
+typedef struct s_philos
 {
+	int				id;
+	int				sleep;
+	int				eaten;
+	int				*dead;
+	size_t			num_philos;
+	size_t			time_die;
+	size_t			time_eat;
+	size_t			time_sleep;
+	size_t			last_meal;
+	pthread_t		thread;
+	pthread_mutex_t	*mealtex;
+	pthread_mutex_t	*eatentex;
+	struct s_table	*table;
+}				t_philos;
 
-}               t_philo;
-
-typedef struct   s_table
+typedef struct s_table
 {
-    
-}               t_table;
+    int				dead_flag;
+	int				num_of_meals;
+	unsigned long	time;
+	pthread_t		table;
+	pthread_mutex_t	mutex;
+	pthread_mutex_t	*deadtex;
+	pthread_mutex_t	*eatentex;
+	pthread_mutex_t	*forks;
+	t_philos		*philos;
+}				t_table;
+
+/* validation_utils.c */
+int		validate_arguments(int ac, char **av);
+size_t	ft_strlen(char *str);
+int		ft_atoi(char *str);
+int		ft_is_not_numeric(char *str);
+
 
 
 #endif
