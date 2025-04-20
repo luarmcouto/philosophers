115082e24680e0bb31b7e33e8d9910a64ec216ac
Lua Rodrigues Monteiro Couto <luarodri@c2r1s4.42porto.com>
2025-04-20 22:11:29 +0100

    chore: update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: luarodri <luarodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 18:41:09 by luarodri          #+#    #+#             */
-/*   Updated: 2025/04/20 20:54:14 by luarodri         ###   ########.fr       */
+/*   Updated: 2025/04/20 21:54:33 by luarodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 int	main(int ac, char **av)
 {
-	t_table		table;
-	t_philos	philosophers[MAX_PHILOS];
+	//t_table		table;
+	//t_philos	philosophers[MAX_PHILOS];
 
 	if (validate_arguments(ac, av))
 		return (1);
-	initialize_table();//TODO
-	initialize_all_philosophers();//TODO
-	start_dinner_simulation();//TODO
-	cleanup_simulation();//TODO
+	//initialize_table();//TODO
+	//initialize_all_philosophers();//TODO
+	//start_dinner_simulation();//TODO
+	//cleanup_simulation();//TODO
 	return (0);
 }
