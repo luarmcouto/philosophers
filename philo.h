33d0f77d3487cca4529c2ac1a3c5537cbe3a5876
Lua Rodrigues Monteiro Couto <luarodri@c2r1s4.42porto.com>
2025-04-20 18:46:00 +0100

    starting philo.h and main.c

---
+++ philo.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luarodri <luarodri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/20 18:37:56 by luarodri          #+#    #+#             */
+/*   Updated: 2025/04/20 18:44:31 by luarodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifdef PHILO_H
+# define PHILO_H
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+typedef struct  s_philo
+{
+
+}               t_philo;
+
+typedef struct   s_table
+{
+    
+}               t_table;
+
+
+#endif
