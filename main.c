dbed39214a75ae3203791a4538a54cd03e65d7e4
Lua Rodrigues Monteiro Couto <luarodri@c2r6s3.42porto.com>
2025-04-21 22:29:25 +0100

    chore: update main.c to include start_dinner_simulation

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: luarodri <luarodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 18:41:09 by luarodri          #+#    #+#             */
-/*   Updated: 2025/04/21 21:51:03 by luarodri         ###   ########.fr       */
+/*   Updated: 2025/04/21 22:28:28 by luarodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	main(int ac, char **av)
 		return (1);
 	initialize_table(philosophers, &table, av);
 	initialize_all_philosophers(&table, philosophers, av);
-	//start_dinner_simulation();//TODO
+	start_dinner_simulation(&table);//TODO
 	//cleanup_simulation();//TODO
 	return (0);
 }
