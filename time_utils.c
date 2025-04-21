226ad3621ed26474b90845e1336a1ab0d6445600
Lua Rodrigues Monteiro Couto <luarodri@c2r6s3.42porto.com>
2025-04-21 21:45:56 +0100

    feat: add time_utils.c for time-related utilities

---
+++ time_utils.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   time_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luarodri <luarodri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/21 21:28:43 by luarodri          #+#    #+#             */
+/*   Updated: 2025/04/21 21:32:58 by luarodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	sleep_for_ms(size_t milliseconds)
+{
+	size_t	start_time;
+
+	start_time = get_current_time();
+	while ((get_current_time() - start_time) < milliseconds)
+		usleep(100);
+	return (0);
+}
+
+size_t	get_current_time(void)
+{
+	struct timeval	time;
+
+	if (gettimeofday(&time, NULL) == -1)
+		write(2, "gettimeofday() error\n", 22);
+	return (time.tv_sec * 1000 + time.tv_usec / 1000);
+}
